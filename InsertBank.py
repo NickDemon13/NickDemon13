490737f1e9473d7f9f565d3fb15cdecd4b9ee991
NickDemon13 <98690566+NickDemon13@users.noreply.github.com>
2022-02-01 14:27:22 +0800

    Update InsertBank.py

--- InsertBank.py
+++ InsertBank.py
@@ -1,5 +1,5 @@
 print('Please Write The Bank Detail Information')
 
-print('No Account    :                          ')
-print('Kind Of Bank  :                          ')
-print('Bank name     :                          ')
+print('No Account    : 03111020232075')
+print('Kind Of Bank  : BANK ISLAM MALAYSIA')
+print('Bank name     : ENCIK WAN MOHAMAD HAFIZAN BIN WAN ABDULL')
