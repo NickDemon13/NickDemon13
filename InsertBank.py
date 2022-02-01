6e6d3c2297b023b57899e2550ebf96dc81d8e50d
NickDemon13 <98690566+NickDemon13@users.noreply.github.com>
2022-02-01 13:47:26 +0800

    Create InsertBank.py

---
+++ InsertBank.py
@@ -0,0 +1,5 @@
+Print('Please Write The Bank Detail Information')
+
+Print('No Account    :                          ')
+Print('Kind Of Bank  :                          ')
+Print('Bank name     :                          ')
