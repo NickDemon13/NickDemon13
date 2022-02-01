cd4bca5fe3cc285cb48f4d4d2c029c0be76fc9cd
NickDemon13 <98690566+NickDemon13@users.noreply.github.com>
2022-02-01 14:22:02 +0800

    Update BankSpamReport.py

--- BankSpamReport.py
+++ BankSpamReport.py
@@ -1,11 +1,12 @@
-Print('Please Choose The Bank Owner Case Type & How Many Report You Want Make')
-Print('#Warning! This action will make this bank number get locked and dealing with the police in the country.')
+print('Please Choose The Bank Owner Case Type & How Many Report You Want Make')
+print('#Warning! This action will make this bank number get locked and dealing with the police in the country.')
 
-Print('1.Gamble Case')
-Print('2.Scam Case')
-Print('3.Fraud Case')
 
-Print('1.Report 50x')
-Print('2.Report 100x')
-Print('3.Report 200x')
-Print('4.Report 300x')
+print('1.Gamble Case')
+print('2.Scam Case')
+print('3.Fraud Case')
+
+print('1.Report 50x')
+print('2.Report 100x')
+print('3.Report 200x')
+print('4.Report 300x')
