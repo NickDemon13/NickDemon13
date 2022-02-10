83ff2bd4465d0f78045132853f8e51e40d086518
NickDemon13 <98690566+NickDemon13@users.noreply.github.com>
2022-02-10 16:11:07 +0800

    Create HotelManage.py

---
+++ HotelManage.py
@@ -0,0 +1,21 @@
+class hotelmanage:
+    
+    def __init__(self, rt='', s=0, p=0, r=0, t=0, a=1000, name='', address='', cindate='', coutdate='', rno=1):
+        
+        print ("\n\n*****WELCOME TO THE HELL*****\n")
+ 
+        self.rt=rt
+ 
+        self.r=r
+ 
+        self.t=t
+ 
+        self.p=p
+ 
+        self.s=s
+        self.a=a
+        self.name=name
+        self.address=address
+        self.cindate=cindate
+        self.coutdate=coutdate
+        self.rno=rno
