8d67d73629077794ea7bff93bf58d4dd7c45bb8f
NickDemon13 <98690566+NickDemon13@users.noreply.github.com>
2022-02-13 14:35:22 +0800

    Update Pasword.py

--- Pasword.py
+++ Pasword.py
@@ -3,7 +3,7 @@ lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 Numbers = "0123456789"
 symbols="[]{}()*;/,._-"
-all = lower + upper +number +symbols
+all = lower + upper +numbers +symbols
 length = 16
 password ="".join(random.samplef (all,length))
 print(password)
