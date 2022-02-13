74e13565fac76cc05ffa58a67cff0f45df225157
NickDemon13 <98690566+NickDemon13@users.noreply.github.com>
2022-02-13 15:05:41 +0800

    Delete InsertBank.py

--- InsertBank.py
+++
@@ -1,5 +0,0 @@
-print('Please Write The Bank Detail Information')
-
-print('No Account    : 03111020232075')
-print('Kind Of Bank  : BANK ISLAM MALAYSIA')
-print('Bank name     : ENCIK WAN MOHAMAD HAFIZAN BIN WAN ABDULL')
